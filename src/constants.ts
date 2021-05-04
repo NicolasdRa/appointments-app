b16f5be1bf0431d87e5d1ef33cb7dadc4f28f0c9
Nicolas <nicolasdirago@gmail.com>
2021-05-04 07:42:10 +0200

    fix for api call in netlify

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
-export const baseURL = 'https:dummyapi.io/data/api/user?limit=10';
+export const baseURL = 'https://dummyapi.io/data/api/user?limit=10';
 export const APP_ID = '608c47a1017d2283dce814a6';
