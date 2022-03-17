3c731123655f8b349a65e3d91978c2e55e72a50a
Nicolas <nicolasdirago@gmail.com>
2022-03-17 17:57:16 +0100

    changed mock api

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
-export const baseURL = 'https://dummyapi.io/data/api/user?limit=10';
-export const APP_ID = '608c47a1017d2283dce814a6';
+export const baseURL = 'https://fake-users6.p.rapidapi.com/';
+export const API_KEY = '312e885d70msh49b3d4dcfdebf37p1945cdjsn1b9050b9edb7';
